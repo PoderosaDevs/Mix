c5f6c3fe3c19abacc4c0e213bf63ce97b6901a99
EdLoth <eduardosram10@gmail.com>
2026-05-21 16:02:45 -0300

    Ajustes na conexão com a api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,13 @@
 import axios from "axios";
 
+// Ele vai tentar buscar a URL das variáveis de ambiente. 
+// Se não encontrar (em dev local), ele usa o localhost como plano de fundo.
+const baseURL = import.meta.env?.VITE_API_URL // Se estiver usando Vite
+  || process.env.REACT_APP_API_URL            // Se estiver usando Create React App
+  || "http://localhost:3000";                 // Seu fallback local
+
 export const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: baseURL,
 });
 
 api.interceptors.request.use((config) => {
